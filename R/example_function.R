5fc15566da9b00821570696535fa73a6beb00689
blaisemariner17 <blaisemariner17@gmail.com>
2024-03-14 13:37:49 -0700

    asdg

---
+++ R/example_function.R
@@ -0,0 +1,16 @@
+#'insert description
+#'
+#' @param regions regions of interest
+#' @param transposons_annotation TE annotation file
+#' 
+#' @param return_classes if TRUE, returns if the region overlaps with transposon classes (LINE1, LINE2, SINE_tRNA, etc.)
+#' @return Function returns region metadata of interest
+#' @export example_function
+
+example_function <- function(regions,
+                                       transposons_annotation
+) {
+  ## insert commands
+  
+  return(return)
+}
